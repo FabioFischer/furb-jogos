ac3c97316dc0ef49c7e2a5ec407edd5d4ad22f3c
Digo <digo_nielson@hotmail.com>
2017-10-10 20:16:16 -0300

    initial game commit

---
+++ unity/Crossing the Ages with Ludwig/Assets/Scripts/Escada.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escada : MonoBehaviour, IUsable
+{
+
+    public void Use()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ unity/Crossing the Ages with Ludwig/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody;
+    private Animator myAnimator;
+    private bool facingRight;
+    public bool OnLadder { get; set; }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        OnLadder = false;
+        facingRight = true;
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        MovimentHandler(horizontal, vertical);
+        Flip(horizontal);
+    }
+
+    private void MovimentHandler(float horizontal, float vertical)
+    {
+        if (OnLadder)
+        {
+            myRigidBody.velocity = new Vector2(horizontal * 10, vertical * 10);
+        }
+
+        myRigidBody.velocity = new Vector2(horizontal * 10, myRigidBody.velocity.y);
+        myAnimator.SetFloat("Speed", Mathf.Abs(horizontal));
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+
+            transform.localScale = scale;
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Escada")
+        {
+            myRigidBody.gravityScale = 0;
+            OnLadder = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Escada")
+        {
+            myRigidBody.gravityScale = 10;
+            OnLadder = false;
+        }
+    }
+}
