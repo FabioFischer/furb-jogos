9378d73aeaab62b08391380c646cce828926da9a
Djonathan <odjonathankrause@gmail.com>
2017-11-14 20:02:53 -0200

    Som ao pular

---
+++ unity/Crossing the Ages with Ludwig/Assets/PlayOnJump.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnJump : MonoBehaviour 
+{
+
+	public AudioSource jumpSound;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			jumpSound.Play();
+		}
+	}
+}
