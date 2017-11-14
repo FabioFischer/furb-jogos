91a4b7edbd7914e701a1301e97311b91498a7517
Djonathan <odjonathankrause@gmail.com>
2017-11-14 21:40:56 -0200

    Menu + Ajuste em músicas

---
+++ unity/Crossing the Ages with Ludwig/Assets/MainMenu/MainMenuButtonsActions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonsActions : MonoBehaviour 
+{
+	/// <summary>
+	/// On button Play click, load Fase1 scene.
+	/// To a scene be loaded, it has to be added on Edit->Build and Settings.
+	/// </summary>
+	public void Play()
+	{
+		SceneManager.LoadScene("Fase1");
+	}
+
+	/// <summary>
+	/// Show details about the game.
+	/// </summary>
+	public void About()
+	{
+
+	}
+
+	/// <summary>
+	/// Exit game on button click.
+	/// </summary>	 	
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
